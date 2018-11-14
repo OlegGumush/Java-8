e8ac57e75885de2b2e6c3ac45c65668064bb02c9
oleg <oleg@DESKTOP-4SIKP0G>
2018-11-14 23:55:45 +0200

    return value from java

---
+++ src/com/Chapter_03_LambdaExpression/Lesson_03/MyInterface.java
@@ -0,0 +1,7 @@
+package com.Chapter_03_LambdaExpression.Lesson_03;
+
+@FunctionalInterface
+public interface MyInterface {
+
+	public abstract boolean test(int n1 , int n2);
+}
---
+++ src/com/Chapter_03_LambdaExpression/Lesson_03/RetureValueFromLambda.java
@@ -0,0 +1,18 @@
+package com.Chapter_03_LambdaExpression.Lesson_03;
+
+public class RetureValueFromLambda {
+
+	public static void main(String[] args) {
+		MyInterface myInterface = (n1,n2) -> {
+			return n1 > n2;
+		};
+		System.out.println(myInterface.test(40, 60));
+	
+		
+		
+		MyInterface myInterface1 = (n1,n2) -> n1 < n2;
+		System.out.println(myInterface1.test(40, 60));
+	
+	
+	}
+}
